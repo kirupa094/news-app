ca5dac596611cde3a5a3288e679bdab08384865d
kiruba094 <kirupakaran094@gmail.com>
2023-05-13 11:04:11 +0530

    routeScreen added

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/route_screen.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'News App',
+      debugShowCheckedModeBanner: false,
+      home: RouterScreen(),
+    );
+  }
+}
